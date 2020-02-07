74e1fab86db82ac5607f8d7a4d6275c4eb2e6d07
kou <1649369473@qq.com>
2020-02-07 10:31:51 +0800

    commit

---
+++ precache-manifest.7610ebd9d882e3c18b9d0c594a18d880.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bb4ba52614630860e4901823db30a3",
+    "url": "/nest-water-tap/index.html"
+  },
+  {
+    "revision": "ad6766a252e3cfa2a4fb",
+    "url": "/nest-water-tap/static/css/main.22a71e13.chunk.css"
+  },
+  {
+    "revision": "f8085b1797d84c99a003",
+    "url": "/nest-water-tap/static/js/2.202a7b17.chunk.js"
+  },
+  {
+    "revision": "99c5655d2432b8199a89e031b7b8d3f7",
+    "url": "/nest-water-tap/static/js/2.202a7b17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad6766a252e3cfa2a4fb",
+    "url": "/nest-water-tap/static/js/main.96c5fead.chunk.js"
+  },
+  {
+    "revision": "f484a20f0c7488c55d84",
+    "url": "/nest-water-tap/static/js/runtime-main.2dd652fa.js"
+  }
+]);
